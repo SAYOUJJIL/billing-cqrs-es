30a6e1e2805125b1ef58ee57c21dbd00bae44b20
AYOUJJIL Soukayna <s.ayoujjil_etu@enset-media.ac.ma>
2022-12-26 12:25:31 +0100

    first commit

---
+++ billing-cqrs-es/inventory-service/src/main/java/ma/enset/inventoryservice/security/KeycloakAdapterConf.java
@@ -0,0 +1,13 @@
+package ma.enset.inventoryservice.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakAdapterConf {
+    @Bean
+    KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
